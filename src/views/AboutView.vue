2923b0b43f392382892550220fd9c33daa839f07
Emanuel Mineda Carneiro <mineda@gmail.com>
2023-03-14 12:54:03 +0000

    Exemplo de diretivas do Vue.js

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,23 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const nome = ref("Nome");
+
+const contador = ref(1);
+
+function incrementar() {
+  contador.value++;
+}
+
+</script>
+
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Bem vindo {{ nome }}</h1>
+    <p><input type="text" v-model="nome"/></p>
+    <p v-if="nome.length > 5">Nome longo</p>
+    <p v-else>Nome curto</p>
+    <p>{{ contador }} <button @click="incrementar">Incrementar</button></p>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
