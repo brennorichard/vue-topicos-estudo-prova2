0a417ed1e94c76a938ea46ea3adb0731849c4d97
Emanuel Mineda Carneiro <mineda@gmail.com>
2023-03-20 13:27:37 +0000

    Post

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,6 +2,8 @@
   <div class="about">
     <h1>Bem vindo {{ nome }}</h1>
     <p><input type="text" v-model="nome"/></p>
+    <p><input type="password" v-model="senha"/></p>
+    <button @click="cadastrar">Cadastrar</button>
     <p v-if="nome.length > 5">Nome longo</p>
     <p v-else>Nome curto</p>
     <p>{{ contador }} <button @click="incrementar">Incrementar</button></p>
@@ -25,6 +27,7 @@ import { ref, onMounted } from 'vue';
 import axios from 'axios';
 
 const nome = ref("Nome");
+const senha = ref("123");
 
 const contador = ref(1);
 
@@ -40,6 +43,21 @@ async function atualizar() {
   try {
     usuarios.value = (await axios.get('https://8080-mineda-springtopicos202-wojfos5znh2.ws-us90.gitpod.io/usuario')).data;
     erro.value = '';
+  }
+  catch(ex) {
+    erro.value = (ex as Error).message;
+  }
+}
+
+async function cadastrar() {
+  try {
+    await axios.post('https://8080-mineda-springtopicos202-wojfos5znh2.ws-us90.gitpod.io/usuario', 
+      {
+        nome: nome.value,
+        senha: senha.value
+      });
+    erro.value = '';
+    atualizar();
   }
   catch(ex) {
     erro.value = (ex as Error).message;
