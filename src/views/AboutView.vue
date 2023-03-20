a6b4393bc2b75cd911811572715af6169aea69cd
Emanuel Mineda Carneiro <mineda@gmail.com>
2023-03-20 13:07:29 +0000

    Requisição GET

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,16 +1,3 @@
-<script setup lang="ts">
-import { ref } from 'vue';
-
-const nome = ref("Nome");
-
-const contador = ref(1);
-
-function incrementar() {
-  contador.value++;
-}
-
-</script>
-
 <template>
   <div class="about">
     <h1>Bem vindo {{ nome }}</h1>
@@ -18,6 +5,50 @@ function incrementar() {
     <p v-if="nome.length > 5">Nome longo</p>
     <p v-else>Nome curto</p>
     <p>{{ contador }} <button @click="incrementar">Incrementar</button></p>
+    <button @click="atualizar">Atualizar</button>
+    <p v-if="erro">{{ erro }}</p>
+    <table>
+      <thead>
+        <td>Id</td>
+        <td>Nome</td>
+      </thead>
+      <tr v-for="usuario in usuarios" :key="usuario.id">
+        <td>{{ usuario.id }}</td>
+        <td>{{ usuario.nome }}</td>
+      </tr>
+    </table>
   </div>
 </template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+
+const nome = ref("Nome");
+
+const contador = ref(1);
+
+const usuarios = ref();
+
+const erro = ref('');
+
+function incrementar() {
+  contador.value++;
+}
+
+async function atualizar() {
+  try {
+    usuarios.value = (await axios.get('https://8080-mineda-springtopicos202-wojfos5znh2.ws-us90.gitpod.io/usuario')).data;
+    erro.value = '';
+  }
+  catch(ex) {
+    erro.value = (ex as Error).message;
+  }
+}
+
+onMounted(() => {
+  atualizar();
+});
+
+</script>
 
